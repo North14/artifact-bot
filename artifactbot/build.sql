defd075979e80711a3693c99d7e8ddf6a95a04fd
North14 <fandersson50@gmail.com>
2022-04-08 13:58:47 +0200

    xd

--- artifactbot/build.sql
+++ artifactbot/build.sql
@@ -3,12 +3,8 @@ CREATE TABLE IF NOT EXISTS pinglist (
   Added INT
 );
 
-INSERT INTO pinglist(Username, Added) VALUES("testuser", 1) ON CONFLICT DO NOTHING;
-
 CREATE TABLE IF NOT EXISTS suggestions (
   Username TEXT PRIMARY KEY,
   Suggestion TEXT,
   Created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO suggestions(Username, Suggestion) VALUES("username123", "This is a suggestion") ON CONFLICT DO NOTHING;
--- artifactbot/main.py
+++ artifactbot/main.py
@@ -1,6 +1,7 @@
 from twitchio.ext import commands
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
+import time
 import logging
 import re
 import sys
@@ -47,11 +48,14 @@ class Bot(commands.Bot):
         super().__init__(token=token, prefix=prefix, initial_channels=initial_channels)
 
     async def event_ready(self):
+        # TODO import variables from disk
         self.logger.info(f'Logged in as | {self.nick}')
         await self.database.connect()
         self.logger.info("Database connected")
         self.scheduler.start()
         self.logger.info("Scheduler started")
+        self.scheduler.add_job(self.database.commit, 'cron', second=0)
+        self.logger.info("Database commit schedule started")
         self.channel: twitchio.Channel = self.get_channel(
             next(iter(initial_channels))
         )
@@ -69,13 +73,9 @@ class Bot(commands.Bot):
         if not ctx.author.name in owners: return
         await ctx.send(f"There are ({len(self.scheduler.get_jobs()):,} jobs in queue")
 
-    @commands.command()
-    async def pings(self, ctx: commands.Context):
-        if not ctx.author.name in owners: return
-        await ctx.send(f"There are {len(self.remind.remind_next)} bajs waiting for ping")
-
-    @commands.command()
-    async def shutdown(self, ctx: commands.Context):
+    @commands.command(name="shutdown", aliases=["reboot", "restart", "update"])
+    async def shutdown_command(self, ctx: commands.Context):
+        # TODO write variables to disk
         if not ctx.author.name in owners: return
         await ctx.send(f"Shutting down")
         self.logger.info("Shutting down from shutdown command")
@@ -83,10 +83,19 @@ class Bot(commands.Bot):
         await self.database.disconnect()
         sys.exit(0)
 
-    @commands.command()
-    async def ping(self, ctx: commands.Context):
+    @commands.command(name="ping", aliases=["info"])
+    async def ping_command(self, ctx: commands.Context):
         cur_time = datetime.now().strftime("%H:%M")
-        await ctx.send(f"pong : {cur_time}")
+        uptime = time.time() - start_time
+        mm, ss = divmod(uptime, 60)
+        uptime_string = f"{int(mm)}m {int(ss)}s"
+        if mm >= 60:
+            hh, mm = divmod(mm, 60)
+            uptime_string = f"{int(hh)}h {int(mm)}m"
+            if hh >= 24:
+                dd, hh = divmod(hh, 24)
+                uptime_string = f"{int(dd)}d {int(hh)}h"
+        await ctx.send(f"Current time: {cur_time} | Uptime: {uptime_string}")
 
     @commands.command()
     async def trigger(self, ctx: commands.Context):
@@ -117,9 +126,16 @@ class Bot(commands.Bot):
         self.remind.movie_list = []
         self.remind.clear_remind_next_cur()
         cur_time = datetime.now().strftime("%H:%M")
+        temp_thingy = False
         for movie in movies:
             movie.append([])
-            if datetime.strptime(cur_time, "%H:%M") < datetime.strptime(movie[1], "%H:%M"):
+            self.logger.info(movie)
+            if not temp_thingy:
+                if datetime.strptime(cur_time, "%H:%M") < datetime.strptime(movie[1], "%H:%M"):
+                    temp_thingy = True
+                else:
+                    self.logger.info(f"{movie[0]} already played")
+            if temp_thingy:
                 if cur_movie_list:
                     for cur_movie in cur_movie_list:
                         if cur_movie[0].lower() == movie[0].lower():
@@ -130,8 +146,6 @@ class Bot(commands.Bot):
                 # TODO
                 hour, minute = movie[1].split(':')
                 self.remind.add_to_remind_next_cur(hour, minute)
-            else:
-                self.logger.info(f"{movie[0]} already played")
         self.logger.info(self.remind.movie_list)
         await ctx.send(f"{len(self.scheduler.get_jobs()):,} jobs loaded")
 
@@ -156,8 +170,15 @@ class Bot(commands.Bot):
         if not ctx.author.name in owners: return
         await self.suggest.print_suggestions()
 
-    @commands.command(name="remind", aliases=["remindme"])
+    @commands.command(name="remind", aliases=["remindme", "pingme"])
     async def reminder(self, ctx: commands.Context):
+        """
+        This is a test
+
+        Docstring
+
+        123
+        """
         msgs = ctx.message.content.split(" ")
         self.logger.debug(msgs)
         try:
@@ -191,10 +212,35 @@ class Bot(commands.Bot):
         except IndexError:
             await ctx.send("Usage: +remind {next|perma|<dlc title>}")
 
+    # @commands.command()
+    # async def usage(self, ctx:commands.Context):
+    #     # print(self.get_command("remind"))
+    #     # print(self.get_command("remind").name)
+    #     # print(self.get_command("remind").full_name)
+    #     # print(self.get_command("remind").aliases)
+    #     # print(self.get_command("remind").params)
+    #     # print(self.get_command("remind").__dict__)
+    #     # print(dir(self.get_command("remind")))
+    #     command_list = {}
+    #     commands = ['remind', 'suggest']
+    #     for command in commands:
+    #         command_list[self.get_command(command).name] = [self.get_command(command).aliases]
+    #     print(command_list)
+
     @commands.command()
-    async def test(self, ctx:commands.Context):
-        table = await self.remind.get_perma_pings()
-        self.logger.info(table)
+    async def debug_python(self, ctx: commands.Context):
+        if not ctx.author.name == "artifactsection": return
+        python_command = " ".join(ctx.message.content.split(" ")[1:])
+        row = eval(python_command)
+        self.logger.info(f"{row}")
+        await ctx.send(f"{row}")
+
+    @commands.command()
+    async def debug_sql(self, ctx: commands.Context):
+        if not ctx.author.name == "artifactsection": return
+        sql_command = " ".join(ctx.message.content.split(" ")[1:])
+        row = await self.database.execute(sql_command)
+        self.logger.info(f"{row}")
 
     @commands.command(name="help", aliases=["commands"])
     async def helplist(self, ctx: commands.Context):
@@ -207,7 +253,6 @@ class Bot(commands.Bot):
                 "list",
                 "trigger",
                 "jobs",
-                "pings",
                 ]
         if ctx.author.name in owners:
             await ctx.send(f"Mod commands: {' '.join(pleb_commands) + ' ' + ' '.join(mod_commands)}")
@@ -215,5 +260,6 @@ class Bot(commands.Bot):
         await ctx.send(f"Pleb commands: {' '.join(pleb_commands)}")
 
 if __name__ == "__main__":
+    start_time = time.time()
     bot = Bot()
     bot.run()
