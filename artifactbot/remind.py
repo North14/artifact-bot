a26e73275330f5df450c4a8ac7d21b3b939cb287
North14 <fandersson50@gmail.com>
2022-04-13 09:07:44 +0200

    small bugfix xd

--- artifactbot/remind.py
+++ artifactbot/remind.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from apscheduler.jobstores.base import JobLookupError
 
 class Remind:
     def __init__(self, bot):
@@ -36,8 +37,12 @@ class Remind:
 
     def clear_remind_next_cur(self):
         self.bot.logger.info(f"Clearing jobs and movie list")
+        self.bot.logger.info(self.remind_next_cur)
         for x in self.remind_next_cur:
-            x.remove()
+            try:
+                x.remove()
+            except JobLookupError:
+                pass
         self.remind_next_cur = []
 
     async def get_perma_pings(self):
@@ -60,19 +65,25 @@ class Remind:
         for perma_name in perma_pings:
             # add perma pings to current remind
             self.remind_next.append(perma_name[0])
-        cur_time = dt.datetime.now().strftime("%H:%M")
+        current = dt.datetime.now().strftime("%H:%M")
+        cur_time = [current, current[1:]] if current.startswith('0') else [current]
         self.bot.logger.info(f"Movie list before ping: {self.movie_list}")
-        if self.movie_list and self.movie_list[0][1] == cur_time:
-            # add movie pings to current remind
-            self.bot.logger.info(f"Pinging for movie {self.movie_list[0][0]}")
-            cur_movie = self.movie_list[0]
-            for username in cur_movie[2]:
-                self.remind_next.append(username)
-            base_msg = f"{cur_movie[0]} started DinkDonk"
-            self.movie_list.pop(0)
+        if self.movie_list:
+            if self.movie_list[0][1] in cur_time:
+                # add movie pings to current remind
+                self.bot.logger.info(f"Pinging for movie {self.movie_list[0][0]}")
+                cur_movie = self.movie_list[0]
+                for username in cur_movie[2]:
+                    self.remind_next.append(username)
+                base_msg = f"{cur_movie[0]} starting DinkDonk"
+                self.movie_list.pop(0)
+            else:
+                self.bot.logger.info("Pinging generic")
+                self.bot.logger.info(f"Current time: {cur_time}, Movie list: {self.movie_list}")
+                base_msg = "Next dlc starting DinkDonk"
         else:
-            self.bot.logger.info(f"Found no movie, pinging generic")
-            base_msg = "Next dlc started DinkDonk"
+            self.bot.logger.info("Found no movie list, pinging generic")
+            base_msg = "Next dlc starting DinkDonk"
         self.remind_next = list(dict.fromkeys(self.remind_next)) # clear duplicate pings
         ping_msg = ""
         for ping in self.remind_next:
